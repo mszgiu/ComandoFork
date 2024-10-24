43fcf3501ebfe4385187c11c3165bba9c8b13611
Giulia Marques <0001068970@senaimgaluno.com.br>
2024-10-24 15:16:35 -0300

    Enviando arquivo

--- ConhendoFork/src/Ex09.java
+++ ConhendoFork/src/Ex09.java
@@ -1,10 +1,38 @@
+
 /*Contagem de Dias do Mês: Peça o mês ao usuário e informe quantos dias 
- * ele tem (considerando anos bissextos).
- */
+ * ele tem.*/
+import java.util.Scanner;
+
 public class Ex09 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+		int mes;
+		int dias = 0;
+
+		System.out.println("Informe um numero do mês (1-12):");
+		mes = scanner.nextInt();
+
+		if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
+			dias = 31;
+		}
+
+		else if (mes == 4 || mes == 6 || mes == 9 || mes == 11) {
+			dias = 30;
+		}
+
+		else if (mes == 2) {
+			dias = 28;
+		}
+
+		else {
+			System.out.println("O mês informado é invalido.");
+		}
+
+		scanner.close();
+
+		System.out.println("O mês " + mes + " tem " + dias + " dias.");
 
 	}
 
